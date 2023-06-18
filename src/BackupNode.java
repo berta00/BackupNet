d60c7f5f403a2d9097aafe75590113d71b0b3118
Marco <bertagnollimarco999@gmail.com>
2023-06-19 00:05:29 +0200

    created the BackupNode class and BackupNodeManager filled by incomplete todo-lists

---
+++ src/BackupNode.java
@@ -0,0 +1,22 @@
+public class BackupNode {
+    private int id;
+    private int[] ip;
+    private String hostname;
+    private int storageCapacity;
+    private int[] backupId;
+
+    public BackupNode(int id, int[] ip){
+        this.id = id;
+        this.ip = ip.clone();
+
+        //todo: request to the host hostname, and check if port of backup service is available
+        //todo: if backup service is available get storageCapacity and backupId
+    }
+
+    public int getId(){return this.id;}
+    public int[] getIp(){return this.ip;}
+    public String getHostname(){return this.hostname;}
+    public int getStorageCapacity(){return this.storageCapacity;}
+    public void setStorageCapacity(int storageCapacity){this.storageCapacity = storageCapacity;}
+
+}
---
+++ src/BackupNodeManager.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+
+public class BackupNodeManager {
+    private LinkedList<BackupNode> currentAvailableNodes; // nodes that are currently available
+    private LinkedList<BackupNode> storageNodes; // nodes that has storage in them (even the inactive ones)
+
+    public BackupNodeManager(){
+        this.currentAvailableNodes = new LinkedList<BackupNode>();
+        this.storageNodes = new LinkedList<BackupNode>();
+
+        updateNodes();
+    }
+
+    // current available nodes
+    public LinkedList<BackupNode> getCurrentAvailableNodes(){
+        updateNodes();
+        return this.currentAvailableNodes;
+    }
+    public int getCurrentAvailableNodesNumber(){
+        updateNodes();
+        return this.currentAvailableNodes.size();
+    }
+    private void updateNodes(){
+        //todo: update the nodes available nodes in the network
+    }
+
+    // storage nodes
+    public LinkedList<BackupNode> getStorageNodes(){
+        return this.currentAvailableNodes;
+    }
+    public int getStorageNodesNumber(){
+        return this.storageNodes.size();
+    }
+
+    // available storage nodes and storage match search
+    public LinkedList<BackupNode> getAvailableStorageNodes(){
+        LinkedList<BackupNode> availableStorageNode = new LinkedList<BackupNode>();
+
+        //todo: compare currenly available node and storage nodes
+
+        return availableStorageNode;
+    }
+    public LinkedList<BackupNode> getAvailableStorageNodesByBackupId(int id){
+        LinkedList<BackupNode> availableStorageNodesWithBackup = new LinkedList<BackupNode>();
+
+        //todo: get all the nodes with a specific backup
+
+        return availableStorageNodesWithBackup;
+    }
+}
